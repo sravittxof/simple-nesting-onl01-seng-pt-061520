0f549ccef6c7378333d0c9e0928074a576653129
Travis Fox <travis.fox90@gmail.com>
2020-06-29 00:44:20 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,8 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
- programmer_hash[:dennis_ritchie][:languages]
- programmer_hash
+ programmer_hash[:dennis_ritchie][:languages][0, 1]
 end
 
 def adding_matz
