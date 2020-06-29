e1b50cbcc1937b3c2031da72c568086a6c706f7f
Travis Fox <travis.fox90@gmail.com>
2020-06-29 00:29:53 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+ programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+ programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
@@ -79,7 +81,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+  programmer_hash[:]
     
 end
 
